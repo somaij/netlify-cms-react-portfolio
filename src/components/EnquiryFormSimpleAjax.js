f158923766ed6b9959a553dec841018c0f6d8d6a
Jason Somai <jsomai458@gmail.com>
2018-12-29 17:12:29 -0500

    changed alert

--- src/components/EnquiryFormSimpleAjax.js
+++ src/components/EnquiryFormSimpleAjax.js
@@ -11,7 +11,7 @@ class Form extends React.Component {
     name: 'Simple Form Ajax',
     subject: '', // optional subject of the notification email
     action: '',
-    successMessage: 'Thanks for your enquiry, we will get back to you soon',
+    successMessage: 'Your email was sent! I will get back to you soon.',
     errorMessage:
       'There is a problem, your message has not been sent, please try contacting us via email'
   }
